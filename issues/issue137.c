78e9508e7b5fdf77ab9d6e94e2cb001558f12cc8
stormalf <stormalf.dev@gmail.com>
2022-10-30 22:12:04 +0100

    trying to manage other extended assembly

--- issues/issue137.c
+++ issues/issue137.c
@@ -1,6 +1,21 @@
-#include <glob.h>
+#include <stdio.h>
+#include <string.h>
+static const char hello[] = "hello, hello!";
 
-int main(void)
+void test_compress(compr, comprLen, uncompr, uncomprLen) char *compr,
+    *uncompr;
+long comprLen, uncomprLen;
 {
+    int err;
+    long len = (long)strlen(hello) + 1;
+
+    strcpy((char *)uncompr, "garbage");
+
+    printf("uncompress(): %s\n", (char *)uncompr);
+}
+
+int main(int argc, char **argv)
+{
+    test_compress(&hello, 14, &hello, 14);
     return 0;
 }
