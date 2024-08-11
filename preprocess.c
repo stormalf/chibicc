736df9799b180f317a19cf7ed3a8b343c34eed06
Sergio Gomes Da Silva <sgomesdasilva@arcadsoftware.com>
2024-08-11 12:27:56 +0200

    ignoring __extension__

--- preprocess.c
+++ preprocess.c
@@ -1441,6 +1441,7 @@ void init_macros(void)
   define_macro("HAVE_ATTRIBUTE_PACKED", "1");
   define_macro("linux", "1");
   define_macro("unix", "1");
+  define_macro("__extension__", "");
   //define_macro("nonnull", "1");
   //====fixing ISS-147 defining the two macros for the linux platform
   define_macro("__ORDER_LITTLE_ENDIAN__", "1234");  
