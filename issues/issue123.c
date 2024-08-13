2d73fccc1a131941497e1a8bcb512366b5aa5b9a
stormalf <stormalf.dev@gmail.com>
2024-08-13 22:35:05 +0200

    fixing exit to 1

--- issues/issue123.c
+++ issues/issue123.c
@@ -1,10 +1,17 @@
-
+#include <sys/types.h>
+#include <sys/ipc.h>
 #include <sys/sem.h>
 
-int main(void)
-{
-    if (sizeof((union semun)))
-        return 0;
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+    struct seminfo *__buf;
+};
 
-    return 0;
+int main() {
+    union semun sem_union;
+    if (sizeof(sem_union) > 0)
+        return 0;
+    return 1;
 }
