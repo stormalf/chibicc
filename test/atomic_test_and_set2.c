0db2eb2e89fe87161dc1cbc2122549601c6acd49
Sergio Gomes Da Silva <sgomesdasilva@arcadsoftware.com>
2024-08-23 15:46:58 +0200

    fixing several issues with extended assembly and adding tests for that

---
+++ test/atomic_test_and_set2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "test.h"
+
+typedef struct pg_atomic_flag {
+    volatile char value;
+} pg_atomic_flag;
+
+static inline bool pg_atomic_test_set_flag_impl(volatile pg_atomic_flag *ptr) {
+    char _res = 1;
+    __asm__ __volatile__(
+        "lock\n"
+        "xchgb %0,%1\n"
+        : "+q" (_res), "+m" (ptr->value)
+        :
+        : "memory");
+    return _res == 0;
+}
+
+int main() {
+    pg_atomic_flag flag = {0};
+    bool result = pg_atomic_test_set_flag_impl(&flag);
+    printf("Test and Set Flag: %d\n", result);
+    Assert(1, result);
+    return 0;
+}
