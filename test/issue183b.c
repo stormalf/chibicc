11291535089aed12b9206e5d38def5e445989376
stormalf <stormalf.dev@gmail.com>
2024-12-07 14:34:04 +0100

    fixing issue ISS-183

--- test/issue183b.c
+++ test/issue183b.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
 int main () {
-    int a = 1;
+    int a = 0;
 
     switch (a)
     {
-    case 0:
+    case 0: 
         int c = 2;
         printf("c = %d\n", c);
         /* code */
         break;
-    case 1:
+    
+    case 1:{
         int c = 1;
         printf("c = %d\n", c);
         break;
-    default:
+    }
+    default: 
         int c = 3;
         printf("c = %d\n", c);
         break;
+    
     }
 
     return 0;
