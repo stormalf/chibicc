5471df8e9bd06a8fe45809814f062d72b211f50a
stormalf <stormalf.dev@gmail.com>
2024-12-09 20:33:50 +0100

    adding test sizeof vla

---
+++ test/sizeof_vla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "test.h"
+
+struct {
+    char c;
+    char arr[];
+} s = {1,2,3};
+
+int main(void) {
+    printf("%zu\n", sizeof(s));
+    ASSERT(1, sizeof(s));
+    return 0;
+}
