fdc360671aed9bb4ddf4159d6ecea2e681549d4a
Stormalf <stormalf.dev@gmail.com>
2026-03-17 22:54:22 +0100

    moving issue to test

--- issues/repro_asm_named_inputs.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int x = 42;
-    int y = 0;
-    asm ("movl %[src], %[dst]" : : [dst]"r"(&y), [src]"r"(x) : "memory");
-    printf("y = %d\n", y);
-    return 0;
-}
